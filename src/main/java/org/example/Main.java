c1907f9ae9272b35f1e2ae2d4755b53adc9d7e94
Afnan-Sayed <afnansayed50@gmail.com>
2024-12-16 11:31:51 +0200

    added User Store pachages and classes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package src.main.java.org.example;
+/*
+UserInformationProvider:
+	- AuthenticationProvider
+	- PersonalProvider
+AuthenticationInformation:
+	- Credentials
+	- CredentialsProvider
+PersonalInformation:
+	- User
+	- UserProvider
+ */
+public class Main {
+
+}
