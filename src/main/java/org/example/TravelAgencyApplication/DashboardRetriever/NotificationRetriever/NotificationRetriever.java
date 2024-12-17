581ea396a1db7598b1093e92809327c0bdf16537
AviationGamerX <76180779+AviationGamerX@users.noreply.github.com>
2024-12-17 22:10:05 +0200

    Intial dashboard notification retriver component Missing a lot of things still, need some more information to continue.

--- src/main/java/org/example/TravelAgencyApplication/DashboardRetriever/NotificationRetriever/NotificationRetriever.java
+++ src/main/java/org/example/TravelAgencyApplication/DashboardRetriever/NotificationRetriever/NotificationRetriever.java
@@ -1,4 +1,32 @@
 package org.example.TravelAgencyApplication.DashboardRetriever.NotificationRetriever;
 
-public class NotificationRetriever {
+import java.util.ArrayList;
+import src.main.java.org.example.TravelAgencyPersistence.NotificationsStore.NotificationProvider.NotificationContentPortal;
+import src.main.java.org.example.TravelAgencyPersistence.NotificationsStore.NotificationContentProvider.Notification;
+
+public class NotificationRetriever { //This class is concerned with retrieving a single user's notifications
+    private ArrayList<Notification> notifications;
+    private int userId;
+    private NotificationContentPortal notificationContent = NotificationContentPortal.getInstance();
+
+    public void retrieveNotifications() {
+        notifications = notificationContent.getNotificationsByUserID(userId);
+    }
+
+    public ArrayList<Notification> getNotifications() {
+        return notifications;
+    }
+
+    //TODO Should we add read and unread?
+//    public ArrayList<Notification> getSeenNotifications() {
+//        ArrayList<Notification> temp = new ArrayList<>(notifications);
+//        temp.retainAll(notificationContent.getSeenNotifications());
+//
+//    }
+
+
+    public NotificationRetriever(int ID) {
+        userId = ID;
+        retrieveNotifications();
+    }
 }
