fba4a59fd4858fab88830760dddaf68018bb8db6
Afnan-Sayed <afnansayed50@gmail.com>
2024-12-20 08:12:25 +0200

    ensured data hiding for class's fileds

--- src/main/java/org/example/TravelAgencyPersistence/UserStore/AuthenticationInformation/AuthenticationCredentials.java
+++ src/main/java/org/example/TravelAgencyPersistence/UserStore/AuthenticationInformation/AuthenticationCredentials.java
@@ -1,4 +1,5 @@
 package org.example.TravelAgencyPersistence.UserStore.AuthenticationInformation;
+//
 public class AuthenticationCredentials
 {
     public String username;
@@ -12,5 +13,45 @@ public class AuthenticationCredentials
         this.username = username;
         this.password = password;
         this.email = email;
+    }
+
+    //setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+
+    //getters
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public int getUserID() {
+        return userID;
     }
 }
