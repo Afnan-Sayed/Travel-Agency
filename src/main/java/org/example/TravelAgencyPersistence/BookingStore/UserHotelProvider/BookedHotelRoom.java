68e5d107971b3ef8aea70ab59b8d70287f32d6db
AviationGamerX <76180779+AviationGamerX@users.noreply.github.com>
2024-12-21 21:57:49 +0200

    edited bookedRoomID to roomID

--- src/main/java/org/example/TravelAgencyPersistence/BookingStore/UserHotelProvider/BookedHotelRoom.java
+++ src/main/java/org/example/TravelAgencyPersistence/BookingStore/UserHotelProvider/BookedHotelRoom.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class BookedHotelRoom {
     public int hotelID;
     public int bookingID;
-    public int bookedRoomID;
+    public int roomID;
     public int roomNum;
     public int userID;
     public Date startDate;
