f12fb8c92d1b06c8b81c51d7c3d307d8f832b6f6
Abd-2004 <abdullahmostafahamad@gmail.com>
2024-12-22 13:59:13 +0200

    fixed typos in messages

--- src/main/java/org/example/TravelAgencyApplication/BookingManagement/EventBooking/EventCancellationTemplate.java
+++ src/main/java/org/example/TravelAgencyApplication/BookingManagement/EventBooking/EventCancellationTemplate.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 
 public class EventCancellationTemplate extends Template {
     public EventCancellationTemplate() {
-        super(11, "###");
+        super(13, "###");
         messages = new ArrayList<>();
 
-        messages.add("Your ticket for event X has been cancelled.");
+        messages.add("Your ticket for event " + getSpecialCharacter() + " has been cancelled.");
         messages.add("لقد تم إلغاء تذكرتك ل" + getSpecialCharacter() + ".");
         messages.add("Votre billet pour l'événement " + getSpecialCharacter() + " a été annulé.");
         messages.add("Ihr Ticket für Veranstaltung " + getSpecialCharacter() + " wurde storniert.");
