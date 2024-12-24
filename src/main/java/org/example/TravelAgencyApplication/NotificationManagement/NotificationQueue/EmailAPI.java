84272b933952ae00858bed03de6192f598f13107
A-ZainEldin <ahmedzaineldin5@gmail.com>
2024-12-24 08:34:39 +0200

    implemented automated message processing through threads, fixed minor naming issues, and added more comments.

--- src/main/java/org/example/TravelAgencyApplication/NotificationManagement/NotificationQueue/EmailAPI.java
+++ src/main/java/org/example/TravelAgencyApplication/NotificationManagement/NotificationQueue/EmailAPI.java
@@ -6,6 +6,7 @@ public class EmailAPI {
     {
         Random random = new Random();
         int randomInt = random.nextInt(1000);
+        //simulates the rare event of the sms server to have issues unrelated to our system
         if(randomInt < 5)
             return 1;// simulation that something went wrong
         return 0;
